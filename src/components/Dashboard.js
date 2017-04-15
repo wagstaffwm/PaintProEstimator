e2fc2fc5c8c5d845d6608df6fb00e48d28037e80
Will Wagstaff <wagstaffwm@Wills-MacBook-Pro.local>
2017-04-15 17:42:14 -0400

    new file

---
+++ src/components/Dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+class Dashboard extends React.Component {
+  render(){
+    return (
+      <View>
+        <Text>
+        Hello Dashboard
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default Dashboard;
